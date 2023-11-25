36121f2829033b75158b0a3af36578390b11b60e
Suraj Gude <Suraj Gude>
2023-11-25 09:23:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useCallback, useEffect, useRef } from 'react'
+import './App.css'
+import PasswordGenerator from './components/passwordGenerator'
+function App() {
+  return (
+    <>
+     <PasswordGenerator/>
+    </>
+  )
+}
+
+export default App
